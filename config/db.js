3f071cd58b4c14bedbea1912a2018b8f3d992874
itsRamit <ece22145@iiitkalyani.ac.in>
2024-06-09 00:20:51 +0530

    mongodb atlas connected

--- config/db.js
+++ config/db.js
@@ -1,6 +1,19 @@
-const { default: mongoose } = require('mongoose');
-const mongooose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const connection = mongoose.createConnection('mongodb://localhost:27017/todo'); 
+// Database
+const database = () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  mongoose
+    .connect(process.env.MONGO_URI, connectionParams)
+    .then(() => console.log("Successfully connected to MongoDB Atlas!"))
+    .catch((error) =>
+      console.error("Error connecting to MongoDB Atlas: ", error)
+    );
+};
+database();
 
-module.exports = connection;
+module.exports = mongoose;
